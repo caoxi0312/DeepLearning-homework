63e50b9b20905873c6c4d13ce4a1aa8222e8b9bf
caoxi0312 <caoxi0312@163.com>
2023-02-07 01:24:13 -0500

    modified basic model

--- assignments/week3/model.py
+++ assignments/week3/model.py
@@ -1,14 +1,17 @@
 from typing import Callable
 
+import torch
+import torch.nn as nn
 
-class MLP:
+
+class MLP(torch.nn.Module):
     def __init__(
         self,
         input_size: int,
         hidden_size: int,
         num_classes: int,
         hidden_count: int = 1,
-        activation: Callable = torch.nn.ReLU,
+        activation: Callable = torch.nn.ReLU(),
         initializer: Callable = torch.nn.init.ones_,
     ) -> None:
         """
@@ -21,7 +24,17 @@ class MLP:
             activation: The activation function to use in the hidden layer.
             initializer: The initializer to use for the weights.
         """
-        ...
+        super(MLP, self).__init__()
+
+        self.activation = activation
+        self.initializer = initializer
+
+        self.layers = nn.ModuleList()
+        for i in range(hidden_count):
+            self.layers += [nn.Linear(input_size, hidden_size)]
+            # self.layers += [self.activation]
+            input_size = hidden_size
+        self.out = nn.Linear(hidden_size, num_classes)
 
     def forward(self, x):
         """
@@ -33,4 +46,10 @@ class MLP:
         Returns:
             The output of the network.
         """
-        ...
+        # flattening
+        # x = x.view(x.shape[0], -1)
+
+        for layer in self.layers:
+            x = layer(x)
+        x = self.out(x)
+        return x
