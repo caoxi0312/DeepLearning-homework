1e72cb70132fc1607680355c99d55e34a37501fa
caoxi0312 <caoxi0312@163.com>
2023-02-18 00:36:52 -0500

    1.gamma

--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -4,7 +4,11 @@ from torch.optim.lr_scheduler import _LRScheduler
 
 
 class CustomLRScheduler(_LRScheduler):
-    def __init__(self, optimizer, last_epoch=-1):
+    """
+    customer class for LRScheduler
+    """
+
+    def __init__(self, optimizer, step_size, last_epoch=-1, gamma=0.9, verbose=False):
         """
         Create a new scheduler.
 
@@ -13,12 +17,21 @@ class CustomLRScheduler(_LRScheduler):
 
         """
         # ... Your Code Here ...
-        super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
+        self.step_size = step_size
+        self.gamma = gamma
+        super(CustomLRScheduler, self).__init__(optimizer, last_epoch, verbose)
 
     def get_lr(self) -> List[float]:
+        """
+        1. use gamma to adjust lr
+        """
         # Note to students: You CANNOT change the arguments or return type of
         # this function (because it is called internally by Torch)
 
         # ... Your Code Here ...
         # Here's our dumb baseline implementation:
-        return [i for i in self.base_lrs]
+        # return [i for i in self.base_lrs]
+
+        if (self.last_epoch == 0) or (self.last_epoch % self.step_size != 0):
+            return [group["lr"] for group in self.optimizer.param_groups]
+        return [group["lr"] * self.gamma for group in self.optimizer.param_groups]
