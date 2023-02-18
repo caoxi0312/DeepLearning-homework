500684d5a16335a33435bc27eb5e36ba36965c5b
caoxi0312 <caoxi0312@163.com>
2023-02-18 13:05:12 -0500

    epoch18

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -7,8 +7,8 @@ from torchvision.transforms import Compose, Normalize, ToTensor
 
 class CONFIG:
     batch_size = 64
-    num_epochs = 14
-    initial_learning_rate = 0.001
+    num_epochs = 18
+    initial_learning_rate = 0.005
     initial_weight_decay = 0
 
     lrs_kwargs = {
