cba65116990300694d19f9860ebfe85497e934c9
caoxi0312 <caoxi0312@163.com>
2023-04-22 12:44:35 -0400

    back to 1st v

--- assignments/05-RL/customagent.py
+++ assignments/05-RL/customagent.py
@@ -2,42 +2,55 @@ import gymnasium as gym
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import numpy as np
+
+
+class Policy(nn.Module):
+    """
+    a neural network that takes observations as input and outputs a probability distribution over actions
+    """
+
+    def __init__(self, obs_dim, hidden_dim, action_dim):
+        super(Policy, self).__init__()
+        self.net = nn.Sequential(
+            nn.Linear(obs_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, action_dim),
+            nn.Softmax(dim=-1),
+        )
+
+    def forward(self, x):
+        return self.net(x)
 
 
 class Agent:
     """
-    A custom agent that uses a neural network to approximate the Q-function.
+    a policy-based agent that learns to maximize the expected return
     """
 
     def __init__(
-        self,
-        action_space: gym.spaces.Discrete,
-        observation_space: gym.spaces.Box,
-        learning_rate: float = 0.1,
-        gamma: float = 0.99,
-        epsilon: float = 1.0,
-        epsilon_decay: float = 0.9995,
-        epsilon_min: float = 0.01,
+        self, action_space: gym.spaces.Discrete, observation_space: gym.spaces.Box
     ):
         self.action_space = action_space
         self.observation_space = observation_space
-        self.learning_rate = learning_rate
-        self.gamma = gamma
-        self.epsilon = epsilon
-        self.epsilon_decay = epsilon_decay
-        self.epsilon_min = epsilon_min
-        self.q_table = np.zeros((observation_space.shape[0], action_space.n))
+
+        obs_dim = observation_space.shape[0]
+        action_dim = action_space.n
+        hidden_dim = 64
+        lr = 1e-3
+
+        self.policy = Policy(obs_dim, hidden_dim, action_dim)
+        self.optimizer = optim.Adam(self.policy.parameters(), lr=lr)
 
     def act(self, observation: gym.spaces.Box) -> gym.spaces.Discrete:
         """
-        returns the action with the highest Q-value for the current state, or a random action with probability
+        uses the policy to choose an action by sampling from this distribution
         """
-        if np.random.rand() < self.epsilon:
-            return self.action_space.sample()
-        state = self._discretize(observation)
-        q_values = self.q_table[state]
-        return np.argmax(q_values)
+        with torch.no_grad():
+            x = torch.FloatTensor(observation).unsqueeze(0)
+            probs = self.policy(x)
+            action = probs.multinomial(1)
+        return action.item()
+        # return self.action_space.sample()
 
     def learn(
         self,
@@ -47,51 +60,14 @@ class Agent:
         truncated: bool,
     ) -> None:
         """
-        updates the Q-value for the (state, action) pair using the Q-learning update rule.
+        updates the policy parameters based on the observed reward and the chosen action
         """
-        state = self._discretize(observation)
-        if terminated or truncated:
-            target = reward
-        else:
-            next_state = self._discretize(observation)
-            next_q_values = self.q_table[next_state]
-            target = reward + self.gamma * np.max(next_q_values)
-        current_q_value = self.q_table[state]
-        current_q_value[action] += self.learning_rate * (
-            target - current_q_value[action]
-        )
-        self.q_table[state] = current_q_value
-        if truncated or terminated:
-            self.epsilon = max(self.epsilon_min, self.epsilon * self.epsilon_decay)
-
-    def _discretize(self, observation):
-        upper_bounds = [
-            self.observation_space.high[0],
-            0.5,
-            self.observation_space.high[2],
-            0.5,
-            self.observation_space.high[4],
-            self.observation_space.high[5],
-        ]
-        lower_bounds = [
-            self.observation_space.low[0],
-            -0.5,
-            self.observation_space.low[2],
-            -0.5,
-            self.observation_space.low[4],
-            self.observation_space.low[5],
-        ]
-        ratios = [
-            (observation[i] + abs(lower_bounds[i]))
-            / (upper_bounds[i] - lower_bounds[i])
-            for i in range(len(observation))
-        ]
-        discretized = [
-            int(round((self.observation_space.nvec[i] - 1) * ratios[i]))
-            for i in range(len(observation))
-        ]
-        discretized = [
-            min(self.observation_space.nvec[i] - 1, max(0, discretized[i]))
-            for i in range(len(observation))
-        ]
-        return tuple(discretized)
+        x = torch.FloatTensor(observation).unsqueeze(0)
+        probs = self.policy(x)
+        log_probs = torch.log(probs)
+        action = probs.multinomial(1)
+        loss = -log_probs[0, action] * reward
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+        # pass
